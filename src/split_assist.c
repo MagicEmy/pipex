8a9a3833989a0b0631fa256fe87af23989f51f24
Emanuela <emanuela.licameli@gmail.com>
2023-10-07 14:50:30 +0200

    Update split_assist.c

--- src/split_assist.c
+++ src/split_assist.c
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	string2 = (unsigned char *) s2;
 	if (n == 0)
 		return (0);
-	while (string1[i] == string2[i] && string1 != '\0' && i < n - 1)
+	while (string1[i] == string2[i] && string1[i] != '\0' && i < n - 1)
 		i++;
 	return (string1[i] - string2[i]);
 }
